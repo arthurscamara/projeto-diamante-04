58f1b557a13e5335179e310019555a7476b577f6
arthurscamara <arthurcamarakai@gmail.com>
2026-09-16 10:13:40 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/campusgigs/api/ProjetoDiamante04Application.java
@@ -0,0 +1,13 @@
+package com.campusgigs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDiamante04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDiamante04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/campusgigs/api/ProjetoDiamante04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusgigs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDiamante04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
